6bfe2dd5c926d9564abcc8a2a9bbe598a03a72bc
jblagorce <github@sgdc.fr>
2024-02-26 15:26:47 +0100

    Initialize repository

---
+++ .github/workflows-scripts/mirror-to-azdo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+AZORG=$AZORG
+AZPROJECT=$AZPROJECT
+AZREPOSITORY=$AZREPOSITORY
+AZUREPAT=$AZUREPAT
+AZUSERNAME=$AZUSERNAME
+
+GITHUBBRANCH=$GITHUBBRANCH
+GITHUBREPOSITORY=$GITHUBREPOSITORY
+GITHUBPAT=$GITHUBPAT
+GITHUBUSERNAME=$GITHUBUSERNAME
+
+git clone "https://$GITHUBUSERNAME:$GITHUBPAT@github.com/$GITHUBREPOSITORY" githubsource
+
+if [ "$?" -ne "0" ]; then
+  echo "Failed to clone the github repository. Exiting."
+  exit 1
+fi
+
+cd githubsource
+
+git checkout $GITHUBBRANCH
+
+git remote add azuredevops "https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZPROJECT/_git/$AZREPOSITORY"
+
+git push --set-upstream azuredevops $GITHUBBRANCH
+
+if [ "$?" -ne "0" ]; then
+    echo "Failed to push branch $GITHUBBRANCH on azure devops."
+fi
+
+cd ..
+
+rm -rf githubsource
